6cb6ee2af253b9bd7d57c5542b6c2dd1c60b2d0e
Sahil Ganbhoj <ganbhojsahil@gmail.com>
2025-11-14 15:59:35 +0530

    try

--- index.js
+++ index.js
@@ -14,4 +14,4 @@ app.use('/api/employees', employeeRoutes);
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://tata-pwa-backend.vercel.app:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on http://tata-pwa-backend.vercel.app`));
