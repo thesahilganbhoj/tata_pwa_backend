c788f373fabb613d64b6cdd4c196d339fa5d6d57
Sahil Ganbhoj <ganbhojsahil@gmail.com>
2025-11-14 16:34:38 +0530

    next commit

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ import authRoutes from './routes/authRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: ['https://tata-pwa-frontend.vercel.app', 'http://localhost:3000'],
+    methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+    credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/employees', employeeRoutes);
@@ -18,5 +22,6 @@ app.get('/', (req, res) => {
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => 
-    console.log(`Server running on http://localhost:${PORT}`));
+    console.log(`Server running on https://tata-pwa-backend.vercel.app`));
 
+export default app;
